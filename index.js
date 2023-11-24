a50177a84a96f3dad36910309439146e64963f8e
Mahadi Hasan <mahadih137@gmail.com>
2023-11-24 17:57:45 +0600

    basic server created with database

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y7tfc1b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("You have successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>
+      <center>Server is running at PORT ${port}</center>
+    </h1>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`app is running at PORT ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+## [Server Repo Link](https://github.com/programming-hero-web-course1/b8a12-server-side-Mahadi-Hasan-Sopon)(https://github.com/programming-hero-web-course1/b8a12-server-side-Mahadi-Hasan-Sopon)
