bee8471036e0c50ab3380c8af90b6b581218b820
Mahadi Hasan <mahadih137@gmail.com>
2023-11-29 23:37:11 +0600

    vercel json added and cors origin added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://inventory-management-design.web.app"],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
@@ -60,7 +60,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // database
     const database = client.db("inventoryManagement");
@@ -527,7 +527,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("You have successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
